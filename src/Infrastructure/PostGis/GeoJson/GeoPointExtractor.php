9ec1a429572a51678918ecc96c6e77e5274cc59b
thePanz <thepanz@gmail.com>
2025-03-13 15:10:20 +0100

    PostGis: add phpstan-types hint for GeoJson point extraction from geometry
    The Brick library might return other objects than the ones defined in the method's signature

--- src/Infrastructure/PostGis/GeoJson/GeoPointExtractor.php
+++ src/Infrastructure/PostGis/GeoJson/GeoPointExtractor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\PostGis\GeoJson;
 
+use Brick\Geo\Geometry;
 use Brick\Geo\LineString;
 use Brick\Geo\MultiPolygon;
 use Brick\Geo\Point;
@@ -17,6 +18,7 @@ final class GeoPointExtractor
     {
         foreach ($multiPolygon->geometries() as $geometryItem) {
             foreach ($geometryItem->getIterator() as $item) {
+                /** @phpstan-var LineString|Geometry $item */
                 if (!$item instanceof LineString) {
                     continue;
                 }
@@ -31,6 +33,7 @@ final class GeoPointExtractor
     public static function extractPointFromPolygon(Polygon $multiPolygon): ?Point
     {
         foreach ($multiPolygon->getIterator() as $item) {
+            /** @phpstan-var LineString|Geometry $item */
             if (!$item instanceof LineString) {
                 continue;
             }
