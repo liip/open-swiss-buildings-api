f4bcd33cd93cf8544b291f45e8bb66f8348bf451
thePanz <thepanz@gmail.com>
2025-03-17 15:51:29 +0100

    AddressSearch: document query string on controller

--- src/Application/Web/Model/AddressSearchFinderQueryString.php
+++ src/Application/Web/Model/AddressSearchFinderQueryString.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Web\Model;
 
+use OpenApi\Attributes as OA;
 use Symfony\Component\Validator\Constraints as Assert;
 
 final class AddressSearchFinderQueryString
@@ -12,6 +13,7 @@ final class AddressSearchFinderQueryString
      * @var non-empty-string
      */
     #[Assert\NotBlank]
+    #[OA\Property(description: 'Search query', example: 'Affoltern')]
     public string $query;
 
     /**
@@ -19,6 +21,12 @@ final class AddressSearchFinderQueryString
      */
     #[Assert\GreaterThanOrEqual(1)]
     #[Assert\LessThanOrEqual(50)]
+    #[OA\Property(
+        description: 'Number of results to return',
+        maximum: 50,
+        minimum: 1,
+        example: 25,
+    )]
     public int $limit = 10;
 
     /**
@@ -26,5 +34,11 @@ final class AddressSearchFinderQueryString
      */
     #[Assert\GreaterThanOrEqual(1)]
     #[Assert\LessThanOrEqual(100)]
+    #[OA\Property(
+        description: 'Lowest matching score of results to return',
+        maximum: 100,
+        minimum: 1,
+        example: 90,
+    )]
     public ?int $minScore = null;
 }
